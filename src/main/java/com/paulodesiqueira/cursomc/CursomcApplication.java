d6c394cc240009f633d61957c60bc9d13501a867
Paulo de Siqueira <paulodesiqueira@gmail.com>
2023-05-19 20:40:19 -0300

    Criação do Projeto

--- src/main/java/com/paulodesiqueira/cursomc/CursomcApplication.java
+++ src/main/java/com/paulodesiqueira/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.example.cursomc;
+package com.paulodesiqueira.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
