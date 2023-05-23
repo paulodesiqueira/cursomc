33adf6221c422483856e92636b965990f400d8e8
Paulo de Siqueira <paulodesiqueira@gmail.com>
2023-05-22 23:51:02 -0300

    Banco de dados h2 e criação automatica de base de dados

--- src/main/java/com/paulodesiqueira/cursomc/domain/Categoria.java
+++ src/main/java/com/paulodesiqueira/cursomc/domain/Categoria.java
@@ -1,11 +1,21 @@
 package com.paulodesiqueira.cursomc.domain;
 
 import java.io.Serializable;
-//import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+@Entity
 public class Categoria implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
